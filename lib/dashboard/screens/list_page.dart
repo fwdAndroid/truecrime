4902cf8d666c23e1ae6a6b6941538ed05ab1815a
fawad kaleem <fwdkaleem@gmail.com>
2023-06-15 01:33:14 +0500

    Home Page Design

--- lib/dashboard/screens/list_page.dart
+++ lib/dashboard/screens/list_page.dart
@@ -10,6 +10,6 @@ class ListPage extends StatefulWidget {
 class _ListPageState extends State<ListPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
